48af2f54e452a33fc0d6bc60c2467d61cc4e4847
loudoweb <loudoweb@gmail.com>
2021-04-14 23:23:55 +0200

    checkstyle fix

--- src/text/UserFontSystem.hx
+++ src/text/UserFontSystem.hx
@@ -24,7 +24,7 @@ class UserFontSystem
     inline public static function getAllFonts(): Array<String> {
         //return Font.enumerateFonts(true);  //waiting for openfl fix
         //TODO sort and return only ttf for max compatibilities
-		return FileSystem.readDirectory('C:/Windows/Fonts');
+        return FileSystem.readDirectory('C:/Windows/Fonts');
     }
 
     /**
@@ -33,9 +33,9 @@ class UserFontSystem
      * @return String return the id of the font
      */
     inline public static function getUserFont(file:String):String {
-		var font = Font.fromFile(FONT_SYSTEM_FOLDER + file);
-		Font.registerFont(font);
-		Assets.cache.setFont(file, font);
+        var font = Font.fromFile(FONT_SYSTEM_FOLDER + file);
+        Font.registerFont(font);
+        Assets.cache.setFont(file, font);
         usedFonts.push(file);
         return font.fontName;
     }
