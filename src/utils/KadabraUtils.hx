89400f53c1db3405bf52880fd509ed91df5f4275
loudoweb <loudoweb@gmail.com>
2021-04-29 15:49:57 +0200

    start a global util (color, size...)

---
+++ src/utils/KadabraUtils.hx
@@ -0,0 +1,21 @@
+package utils;
+
+import feathers.graphics.FillStyle;
+
+class KadabraUtils
+{
+
+
+    public static var HEADER_FILL = SolidColor(0x262626);
+    public static var CANVAS_FILL = SolidColor(0x333333);
+    public static var WINDOW_FILL = SolidColor(0x404040);
+    public static var SCENE_FILL = SolidColor(0x808080);
+
+    inline public static var KADABRA_COLOR = 0xEB7D42;
+    public static var HOVER_FILL = SolidColor(KADABRA_COLOR);
+
+    inline public static var HEADER_THICKNESS = 30;
+
+    public static var DEBUG_RED = SolidColor(0xFF0000);
+    public static var DEBUG_BLUE = SolidColor(0x0000FF);
+}
